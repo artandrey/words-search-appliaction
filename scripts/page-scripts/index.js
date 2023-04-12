f3f22fada8020bc5be6d7314456f44c60e84baf4
Artandrey <artandrey7777@gmail.com>
2023-04-12 19:22:16 +0300

    added file drop upload way

--- scripts/page-scripts/index.js
+++ scripts/page-scripts/index.js
@@ -30,12 +30,71 @@ class FileIndicationText extends Component {
   }
 }
 
+class DropableFileArea extends Component {
+  constructor(id, options) {
+    super(id, options);
+    this.element.addEventListener('dragenter', this.handleDragEtner.bind(this));
+    this.element.addEventListener('dragover', this.handleDragOver.bind(this));
+    this.element.addEventListener('dragleave', this.handleDragLeave.bind(this));
+    this.element.addEventListener('drop', this.handleDrop.bind(this));
+  }
+
+  handleDragLeave() {
+    this.element.classList.remove('active');
+  }
+
+  handleDragEtner() {
+    this.element.classList.add('active');
+  }
+
+  handleDrop(event) {
+    event.preventDefault();
+    event.stopPropagation();
+    this.element.classList.remove('active');
+
+    const files = event.dataTransfer.files;
+    if (!this.#checkFiels(files)) {
+      alert('Invalid file format');
+      return;
+    }
+
+    this.onFileSelect(files);
+  }
+
+  //this method may be overriden
+  onFileSelect(files) {}
+
+  #checkFiels(files) {
+    const { single, format } = this.options;
+    if (!files.length) return false;
+    if (single && files.length !== 1) return false;
+    if (format) {
+      return [...files].every((file) => file.type === format);
+    }
+    return true;
+  }
+
+  handleDragOver(event) {
+    event.preventDefault();
+    event.stopPropagation();
+
+    event.dataTransfer.dropEffect = 'copy';
+  }
+}
+
 class FileInput extends Component {
   constructor(id, options) {
     super(id, options);
 
-    const { indicationId } = this.options;
+    const { indicationId, dropableAreaId } = this.options;
     this.indicationTextComponent = new FileIndicationText(indicationId);
+    this.dropableArea = new DropableFileArea(dropableAreaId, {
+      single: true,
+      format: 'text/plain',
+    });
+
+    this.dropableArea.onFileSelect = this.#handleFileDroped.bind(this);
+
     this.element.addEventListener('change', this.#handleChange.bind(this));
     window.addEventListener(
       'load',
@@ -47,13 +106,22 @@ class FileInput extends Component {
     );
   }
 
+  #handleFileDroped(fileList) {
+    this.#onFileSelect(fileList);
+  }
+
+  #onFileSelect(fileList) {
+    this.element.files = fileList;
+    const file = fileList[0];
+    this.indicationTextComponent.setFileInfo(file.name, file.size);
+  }
+
   #handleChange(event) {
     const file = event.target.files[0];
     if (!file) {
       this.indicationTextComponent.resetFileInfo();
       return;
     }
-    this.indicationTextComponent.setFileInfo(file.name, file.size);
   }
 }
 
@@ -97,4 +165,4 @@ class UserInputForm extends FormComponent {
 }
 
 new UserInputForm('text-form');
-new FileInput('file-input', { indicationId: 'filename' });
+new FileInput('file-input', { indicationId: 'filename', dropableAreaId: 'file-dropable-area' });
